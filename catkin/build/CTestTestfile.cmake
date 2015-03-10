191607ec716af27c9be0f03dc0d64c6b18444fa1
eigendreams <jakob.culebro@gmail.com>
2015-03-10 17:58:04 -0500

    inc

---
+++ catkin/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/viki/curso_ros/catkin/src
+# Build directory: /home/viki/curso_ros/catkin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(test)
---
+++ catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/viki/curso_ros/catkin/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/viki/curso_ros/catkin/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/viki/curso_ros/catkin/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/viki/curso_ros/catkin/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/viki/curso_ros/catkin/devel/lib:/home/viki/curso_ros/catkin/devel/lib/i386-linux-gnu:/opt/ros/indigo/lib/i386-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/viki/curso_ros/catkin/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/viki/curso_ros/catkin/devel/lib/pkgconfig:/home/viki/curso_ros/catkin/devel/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/i386-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/viki/curso_ros/catkin/build"
+export PYTHONPATH="/home/viki/curso_ros/catkin/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/viki/curso_ros/catkin/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/viki/curso_ros/catkin/src:$ROS_PACKAGE_PATH"
