f6ddd274d936e0fc676f27ac426cec480aebf6a3
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-03-10 16:55:49 -0600

    incremental

---
+++ catkin/src/test/scripts/talker.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+class Talker:
+        def __init__(self, node_name = 'talker'):
+                rospy.init_node(node_name)
+                self.nodename = rospy.get_name()
+                rospy.loginfo("%s", self.nodename)
+                self.rate = 10
+                self.pub = rospy.Publisher("talker", String)
+        def update(self):
+                self.pub.publish("Hello World!")
+        def spin(self):
+                r = rospy.Rate(self.rate)
+                while not rospy.is_shutdown():
+                        self.update()
+                        r.sleep()
+
