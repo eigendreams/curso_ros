e37190868e82bb4cbfbc9c1a264cbe4e13ccc7ea
Jakob Culebro Reyes <jakob.culebro@gmail.com>
2015-03-12 15:28:29 -0600

    incremental

--- catkin/src/test/scripts/talker.py
+++ catkin/src/test/scripts/talker.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
+
 class Talker:
         def __init__(self, node_name = 'talker'):
                 rospy.init_node(node_name)
                 self.nodename = rospy.get_name()
-                rospy.loginfo("%s", self.nodename)
-                self.rate = 10
-                self.pub = rospy.Publisher("talker", String)
-        def update(self):
-                self.pub.publish("Hello World!")
+                rospy.loginfo("Started node %s", self.nodename)
+                self.rate = rospy.Rate(10)
+                self.pub = rospy.Publisher("chatter", String)
         def spin(self):
-                r = rospy.Rate(self.rate)
                 while not rospy.is_shutdown():
-                        self.update()
-                        r.sleep()
+                        self.hello_str = "Hello World %s" % rospy.get_time()
+                        rospy.loginfo(self.hello_str)
+                        self.pub.publish(self.hello_str)
+                        self.rate.sleep()
 if __name__ == "__main__":
         talker = Talker()
         talker.spin()
